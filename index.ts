cd6922d5e3d5b23452c46d8863c3add4b78f1658
cisaacson <cory.m.isaacson@gmail.com>
2018-04-19 20:18:34 -0600

    yarn run test works now, used export.module = leveldown syntax in index.ts and reorganized the output directories

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import {KuduScan, KuduInsert, KuduScanResult} from './ts/kududown-schema';
 import {KUDUDOWN_SCHEMA_TYPES} from './ts/kududown-schema-types';
 import {KuduDownSchemaAvscMap} from './ts/kududown-avsc-obj';
+// import * as leveldown from "./leveldown";
+// console.log(`leveldown typeof: ${typeof leveldown}`);
+const leveldown = require('./leveldown');
 
-export const leveldown = require("./leveldown");
-export {KuduScan, KuduInsert, KuduScanResult, KUDUDOWN_SCHEMA_TYPES, KuduDownSchemaAvscMap};
+export {KuduScan, KuduInsert, KuduScanResult, KUDUDOWN_SCHEMA_TYPES,
+  KuduDownSchemaAvscMap, leveldown};
+
+module.exports = leveldown;
