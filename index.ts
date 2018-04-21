75a4ec521d8a54bf17c2544724a8190c9323dbc0
cisaacson <cory.m.isaacson@gmail.com>
2018-04-21 13:23:58 -0400

    Add quicktest of module; anything that loads the library is commented for now; need to refactor names in separate branch

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import * as kududownSchema from './ts/kududown-schema';
 import {KUDUDOWN_SCHEMA_TYPES} from './ts/kududown-schema-types';
 import {KuduDownSchemaAvscMap} from './ts/kududown-avsc-obj';
-// import * as leveldown from "./leveldown";
-// console.log(`leveldown typeof: ${typeof leveldown}`);
-const leveldown = require('./leveldown');
+
+console.log(`Top of index before kududown load`);
+// Get the levelDOWN function for export, which should be exported as leveldown
+// The tests require the name `leveldown` but since consumers will use
+// both the real `leveldown` and this module this will not work. It means
+// changing all the tests. Therefore, this is renamed to `kududown` so
+// the export is a different name.
+
+// Must be commented until the leveldown.node loads.
+// And we should probably change all of the names to kududown.
+//const kududown = require('./leveldown').levelDOWN;
 
 export {kududownSchema, KUDUDOWN_SCHEMA_TYPES,
-  KuduDownSchemaAvscMap, leveldown};
+  KuduDownSchemaAvscMap};
 
-module.exports = leveldown;
+// This was a hack, it overrides module.exports which means all of the
+// typescript generated exports are gone.
+//module.exports = leveldown;
