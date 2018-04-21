22188b42a827f803d4813223a0b093b551eeeb69
cisaacson <cory.m.isaacson@gmail.com>
2018-04-21 12:35:27 -0400

    export kududownSchema; be sure to recompile and check in *.js files to git so that project can be exported properly

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var kududownSchema = require("./ts/kududown-schema");
+exports.kududownSchema = kududownSchema;
 var kududown_schema_types_1 = require("./ts/kududown-schema-types");
 exports.KUDUDOWN_SCHEMA_TYPES = kududown_schema_types_1.KUDUDOWN_SCHEMA_TYPES;
 var kududown_avsc_obj_1 = require("./ts/kududown-avsc-obj");
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import {KuduScan, KuduInsert, KuduScanResult} from './ts/kududown-schema';
+import * as kududownSchema from './ts/kududown-schema';
 import {KUDUDOWN_SCHEMA_TYPES} from './ts/kududown-schema-types';
 import {KuduDownSchemaAvscMap} from './ts/kududown-avsc-obj';
 // import * as leveldown from "./leveldown";
 // console.log(`leveldown typeof: ${typeof leveldown}`);
 const leveldown = require('./leveldown');
 
-export {KuduScan, KuduInsert, KuduScanResult, KUDUDOWN_SCHEMA_TYPES,
+export {kududownSchema, KUDUDOWN_SCHEMA_TYPES,
   KuduDownSchemaAvscMap, leveldown};
 
 module.exports = leveldown;
